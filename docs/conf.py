9d191db7e5213b27e613f0b01e38e371f9a269ae
estilag <38683710+estilag@users.noreply.github.com>
2022-05-19 18:08:52 +0200

    Logo changes

--- docs/conf.py
+++ docs/conf.py
@@ -131,7 +131,7 @@ html_theme = 'default'
 html_static_path = ['_static']
 
 #Add the logo 
-html_logo = "_static/orion_matrix_logos_St.png"
+html_logo = "_static/ohejp_logo.png"
 html_theme_options = {
     'logo_only': True,
     'display_version': False,
